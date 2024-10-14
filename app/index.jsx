fb820104bab886c0dde5f0ba76e90ac5e3bab4b0
Mohammed Shoaib <shoaibkkhn50665@gmail.com>
2024-10-14 22:06:26 +0100

    React native video AI platform

---
+++ app/index.jsx
@@ -0,0 +1,21 @@
+import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+   <View style={styles.container}>
+    <Text>Roar!</Text>
+    <StatusBar style="auto"/>
+   </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+   flex: 1,
+   backgroundColor: '#fff',
+   alignItems: 'center',
+   justifyContent: 'center',
+  },
+});
+
